3183ca94db6a5b599d38e84123a33fedb6bd325f
NileshGule <vn_nilesh@yahoo.com>
2018-05-16 23:23:04 +0800

    updated path to kubernetes manifest files

--- Powershell/deployTechTalks-AKS-ManagedSQL.ps1
+++ Powershell/deployTechTalks-AKS-ManagedSQL.ps1
@@ -11,32 +11,32 @@ if($ProvisionAKSCluster)
 }
 
 Write-Host "Starting deployment of TechTalks application and services" -ForegroundColor Yellow
-Set-Location ~/projects/ABC2018/Kubernetes-ManagedSQL
+Set-Location ~/projects/VoxxedDaysSG-2018/k8s/AKS-ManagedSQL
 
 Write-Host "Creating namespace"  -ForegroundColor Yellow
 kubectl apply --filename 00_ABCNamespace.yml
 Write-Host "Namespace created successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying RabbitMQ service" -ForegroundColor Yellow
-Set-Location ~/projects/ABC2018/Kubernetes-ManagedSQL/RabbitMQ
+Set-Location ~/projects/VoxxedDaysSG-2018/k8s/AKS-ManagedSQL/RabbitMQ
 kubectl apply --recursive --filename . 
 
 Write-Host "RabbitMQ service deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying Tech Talks API service" -ForegroundColor Yellow
-Set-Location ~/projects/ABC2018/Kubernetes-ManagedSQL/TechTalksAPI
+Set-Location ~/projects/VoxxedDaysSG-2018/k8s/AKS-ManagedSQL/TechTalksAPI
 kubectl apply --recursive --filename . 
 
 Write-Host "Tech talks API service deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying Tech Talks web frontend" -ForegroundColor Yellow
-Set-Location ~/projects/ABC2018/Kubernetes-ManagedSQL/TechTalksWeb
+Set-Location ~/projects/VoxxedDaysSG-2018/k8s/AKS-ManagedSQL/TechTalksWeb
 kubectl apply --recursive --filename . 
 
 Write-Host "Tech talks web frontend deployed successfully" -ForegroundColor Cyan
 
 Write-Host "Deploying Tech Talks Processor" -ForegroundColor Yellow
-Set-Location ~/projects/ABC2018/Kubernetes-ManagedSQL/TechTalksProcessor
+Set-Location ~/projects/VoxxedDaysSG-2018/k8s/AKS-ManagedSQL/TechTalksProcessor
 kubectl apply --recursive --filename . 
 
 Write-Host "Tech talks Processor deployed successfully" -ForegroundColor Cyan
