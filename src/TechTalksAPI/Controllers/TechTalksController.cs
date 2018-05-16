a1152729770bfe736f6806ef16a3a269214e7868
NileshGule <vn_nilesh@yahoo.com>
2018-05-16 22:33:46 +0800

    Added message handling

--- src/TechTalksAPI/Controllers/TechTalksController.cs
+++ src/TechTalksAPI/Controllers/TechTalksController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TechTalksAPI.Messaging;
-using TechTalksAPI.Models;
 using TechTalksModel;
 using TechTalksModel.DTO;
 
